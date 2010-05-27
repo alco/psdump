9e3affef46b79028ab3aaf3dea5be277cb23a7fe
Alexei Sholik <alcosholik@gmail.com>
2010-05-27 20:23:09 +0300

    Include getopt implementation for vcproj.

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@
 #include "formatter/JsonFormatter.h"
 
 #include <stdio.h>
+#include <ctype.h>
 
 #if !defined(_MSC_VER)
 #	include <unistd.h>
+#else
+#	include "win32/getopt.h"
 #endif
 
 
@@ -20,13 +23,11 @@ typedef enum {
 
 int main(int argc, char **argv)
 {
+  int option;
   char *f_value = NULL;
   char *o_value = NULL;
 
   OutputFormat output_format = kOutputFormatText;
-
-#if !defined(_MSC_VER)
-  int option;
 
   opterr = 0;
 
@@ -54,9 +55,6 @@ int main(int argc, char **argv)
       abort();
     }
   }
-#else
-  int optind = 1;
-#endif // #if !defined(_MSC_VER)
 
   if (optind >= argc) {
     printf("Usage: psdump [-f FORMAT] [-o OUTPUT_PATH] file ...\n");
