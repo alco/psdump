2158054ac05a4ad02708fdd21036e4ad5b8242df
Alexei Sholik <alcosholik@gmail.com>
2010-05-27 17:38:52 +0300

    Revamp main.cpp and add a basic command line interface with options.

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,91 @@
 #include <stdio.h>
 
 
+typedef enum {
+  kOutputFormatText,
+  kOutputFormatXml,
+  kOutputFormatJson,
+} OutputFormat;
+
+
 int main(int argc, char **argv)
 {
-	if (argc < 2) {
-		printf("Usage: psdump <PSD file name>");
-		return -1;
-	}
+  int option;
+  char *f_value = NULL;
+  char *o_value = NULL;
 
-	PsdParser parser(argv[1]);
-	Document *doc = parser.parse();
+  OutputFormat output_format = kOutputFormatText;
 
-	//	TextFormatter text_dumper;
-	// 	text_dumper.dump_doc_to_file(doc, stdout);
+  opterr = 0;
 
-	//      JsonFormatter json_dumper;
-	//     	json_dumper.dump_doc_to_file(doc, stdout);
+  while ((option = getopt(argc, argv, "f:o:")) != -1) {
+    switch (option) {
+    case 'f':
+      f_value = optarg;
+      break;
 
-		XmlFormatter xml_dumper;
-		xml_dumper.dump_doc_to_file(doc, stdout);
+    case 'o':
+      o_value = optarg;
+      break;
 
-	delete doc;
+    case '?':
+      if (optopt == 'f' || optopt == 'o')
+	fprintf(stderr, "Missing argument for option -%c.\n", optopt);
+      else if (isprint(optopt))
+	fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+      else
+	fprintf(stderr, "Unknown option character ''\\x%x'.\n", optopt);
+      return 1;
 
-	return 0;
+    default:
+      fprintf(stderr, "Uknown option value '%c'", option);
+      abort();
+    }
+  }
+
+  if (optind >= argc) {
+    printf("Usage: psdump [-f FORMAT] [-o OUTPUT_PATH] file ...\n");
+    return 2;
+  }
+
+  if (f_value) {
+    if (strcmp(f_value, "xml") == 0) {
+      output_format = kOutputFormatXml;
+    } else if (strcmp(f_value, "json") == 0) {
+      output_format = kOutputFormatJson;
+    }
+  }
+
+  TextFormatter *formatter;
+
+  switch (output_format) {
+  case kOutputFormatXml:
+    formatter = new XmlFormatter();
+    break;
+
+  case kOutputFormatJson:
+    formatter = new JsonFormatter();
+    break;
+
+  default:
+    formatter = new TextFormatter();
+  }
+
+  for (int index = optind; index < argc; ++index) {
+    PsdParser parser(argv[index]);
+    Document *doc = parser.parse();
+
+    if (!doc) {
+      fprintf(stderr, "Failed to open file '%s'.\n", argv[index]);
+      continue;
+    }
+
+    formatter->dump_doc_to_file(doc, stdout);
+
+    delete doc;
+  }
+
+  delete formatter;
+
+  return 0;
 }
