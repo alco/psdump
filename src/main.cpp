6d6eb1ad4ee317fa2164aa765888cafcac528d43
Alexei Sholik <alcosholik@gmail.com>
2010-05-28 00:52:04 +0300

    Add code for dealing the '-o' option, i.e. output files and dirs.

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,14 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <sys/stat.h>
+
+#include <string>
 
 #if !defined(_MSC_VER)
-#	include <unistd.h>
+#  include <unistd.h>
 #else
-#	include "win32/getopt.h"
+#  include "win32/getopt.h"
 #endif
 
 
@@ -61,6 +64,8 @@ int main(int argc, char **argv)
     return 2;
   }
 
+  /* Determine which format to use for output. */
+
   if (f_value) {
     if (strcmp(f_value, "xml") == 0) {
       output_format = kOutputFormatXml;
@@ -84,6 +89,40 @@ int main(int argc, char **argv)
     formatter = new TextFormatter();
   }
 
+  /* Check if an output file or directory has been specified. */
+
+  char *output_dirname = NULL;
+  char *output_filename = NULL;
+
+  if (o_value) {
+    // Check if its a file or a directory
+    struct stat stat_buffer;
+    int type = stat(o_value, &stat_buffer);
+
+    if (S_ISDIR(stat_buffer.st_mode)) {
+      output_dirname = o_value;
+    } else {
+      output_filename = o_value;
+    }
+  }
+
+  /* Parse actual filenames and print the output. */
+
+  FILE *output_file = stdout;
+
+  if (o_value) {
+    if (output_dirname) {
+      output_file = NULL;
+    } else {
+      output_file = fopen(output_filename, "w");
+    }
+  }
+
+  std::string dir_name;
+  if (output_dirname) {
+    dir_name = output_dirname;
+  }
+
   for (int index = optind; index < argc; ++index) {
     PsdParser parser(argv[index]);
     Document *doc = parser.parse();
@@ -93,9 +132,49 @@ int main(int argc, char **argv)
       continue;
     }
 
-    formatter->dump_doc_to_file(doc, stdout);
+    if (!output_file) {
+      std::string dir_name(output_dirname);
+      dir_name += "/";
+
+      std::string file_name(argv[index]);
+      std::string::size_type slash_index = file_name.rfind('/');
+      if (slash_index == std::string::npos) {
+	slash_index = file_name.rfind('\\');
+      }
+      if (slash_index != std::string::npos) {
+	file_name.erase(0, slash_index + 1);
+      }
+
+      printf("Filename: %s\n", file_name.c_str());
+
+      dir_name += file_name;
+      std::string::size_type dot_index = dir_name.rfind('.');
+      char *format_string;
+      if (!f_value || strcmp(f_value, "text") == 0)
+	format_string = "txt";
+      else
+	format_string = f_value;
+      dir_name.replace(dot_index + 1, dir_name.size() - dot_index - 1, format_string);
+
+      printf("Dirname: %s\n", dir_name.c_str());
+
+      output_file = fopen(dir_name.c_str(), "w");
+    }
+
+    formatter->dump_doc_to_file(doc, output_file);
+
+    if (output_dirname) {
+      fclose(output_file);
+      output_file = NULL;
+    } else if (index + 1 < argc) {
+      fprintf(output_file, "\n");
+    }
 
     delete doc;
+  }
+
+  if (output_filename) {
+    fclose(output_file);
   }
 
   delete formatter;
